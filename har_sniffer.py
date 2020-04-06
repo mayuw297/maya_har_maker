8ac69fd17c0caea846a56f52fad7ff632d813f99
mayuw297 <maya.wineman@gmail.com>
2020-04-06 18:40:43 +0300

    added resources, implemented basic sniffer with colors that shows ack and seq on moomoo packets

--- har_sniffer.py
+++ har_sniffer.py
@@ -1,27 +1,41 @@
+import _thread
 import scapy
 from scapy.layers.inet import IP, TCP
 from scapy.sendrecv import sniff
 
-FACEBOOK_IP = "157.240.20.35"
+MOMO_IP = "158.106.133.146"
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 packs = []
-def filterFcebook(pack):
-    return IP in pack and (pack[IP].src==FACEBOOK_IP or pack[IP].dst==FACEBOOK_IP)
-
-def sniffPacks():
+def filterSite(pack):
     global packs
-    packs = sniff( lfilter=filterFcebook,count = 20)
+    if scapy.packet.Raw in pack:
+        print("LEN: "+bcolors.OKBLUE+str(len(pack[scapy.packet.Raw]))+bcolors.ENDC)
 
+        if IP in pack and pack[IP].dst == MOMO_IP:
+            print("ACK: "+bcolors.OKGREEN + str(pack[TCP].ack) + bcolors.ENDC)
+            packs+=pack
+            print("SEQ: "+str(pack[TCP].seq), pack[scapy.packet.Raw])
+        if IP in pack and pack[IP].src == MOMO_IP:
+            print("ACK: "+bcolors.OKGREEN + str(pack[TCP].ack) + bcolors.ENDC)
+            print("SEQ: "+bcolors.WARNING + str(pack[TCP].seq)+str(pack[scapy.packet.Raw])+ bcolors.ENDC)
+def sniffPacks():
+    sniff( lfilter=filterSite)
 
-def write():
-    pass
+def sortAcks():
+    global packs
 def main():
     global packs
-
     sniffPacks()
-    for p in packs:
-        if scapy.packet.Raw in p:
-            print (p[scapy.packet.Raw])
-        print(p[TCP].seq,p[TCP].ack)
+
+
 
 if __name__ =="__main__":
     main()
